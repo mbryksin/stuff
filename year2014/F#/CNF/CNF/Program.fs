a6ee237f5ca9dec2d43c461307062d33d931eaf9
mbryksin <matvey.bryksin@gmail.com>
2014-10-22 01:04:14 +0400

    last steps

--- year2014/F#/CNF/CNF/Program.fs
+++ year2014/F#/CNF/CNF/Program.fs
@@ -10,15 +10,13 @@ type private Character(name: string) =
     member x.IsVariable = Char.IsUpper(name.[0])
     member x.IsTerminal = not x.IsVariable
 
-//!!!!
-    member x.CompateTo (y : Character) = 
-            if   name > y.Value then 1
-            elif name < y.Value then -1
-            else 0
-
     interface IComparable with
-        member x.CompareTo y = x.CompateTo (y :?> Character)
-//!!!!
+        member x.CompareTo y = x.Compare (y :?> Character)
+
+    member x.Compare (y : Character) = 
+        if name > y.Value then 1
+        else if name < y.Value then -1
+        else 0
 
 type private Production(left: Character, right: Character list) =
     member x.Left = left
@@ -26,24 +24,22 @@ type private Production(left: Character, right: Character list) =
     override x.ToString() = left.Value + " -> " + List.foldBack (fun (x:Character) (acc:string) -> x.Value + " " + acc) right ""
 
     interface IComparable with
-            member x.CompareTo y =
-                let rec compareLists (tail1 : Character list) (tail2 : Character list) =
-                    match tail1, tail2 with
-                    | []     , []      -> 0
-                    | _      , []      -> 1
-                    | []     , _       -> -1
-                    | t1::ts1, t2::ts2 -> let compareT = t1.CompateTo(t2)
-                                          if compareT <> 0 then compareT
-                                          else compareLists ts1 ts2
-                
-                let y = y :?> Production
-                compareLists (x.Left::x.Right) (y.Left::y.Right)
+        member x.CompareTo y =
+            let rec compareLists (t1 : Character list) (t2 : Character list) =
+                match t1, t2 with
+                | [],[] -> 0
+                | _,[] -> 1
+                | [],_ -> -1
+                | x::xs, y::ys -> let res = x.Compare(y)
+                                  match res with
+                                  | 0 -> compareLists xs ys
+                                  | _ -> res
+            compareLists (x.Left::x.Right) ((y :?> Production).Left::(y :?> Production).Right)
 
     override x.Equals y =
         match y with
         | :? Production as y -> x.Left = y.Left && x.Right = y.Right
-        | _               -> false
-//!!!!
+        | _ -> false
 
 type private Grammar(start: Character, productions: Production list) =
     member x.Start = start
@@ -136,32 +132,28 @@ type private Grammar(start: Character, productions: Production list) =
         new Grammar(g.Start, productions)
 
 
-    //---
     static member deleteChains(g:Grammar) =
-//!!!!!
         let deleteProduction (prod:Production) (productions:Production list) =
-            let rec createNewTails (paths:Character list list) (prods:Production list) acc =
+            let rec createProductions (paths:Character list list) (prods:Production list) acc =
                 match paths with
-                | (curr::prev)::ps -> 
-                    let tails1, tailsN = prods
-                                        |> List.filter (fun p -> p.Left = curr)
+                | (x::y)::ps -> 
+                    let t1, t2 = List.filter (fun (p:Production) -> p.Left = x) prods
                                         |> List.map (fun p -> p.Right)
                                         |> List.partition (fun t -> t.Length = 1 && t.Head.IsVariable)      
-                    let newPaths = tails1
-                                |> List.map List.head
-                                |> List.filter (fun c -> not <| List.exists ((=)c) (curr::prev))
-                                |> List.map (fun c -> c::curr::prev)
-                                |> fun newPs -> newPs @ ps
+                    let paths = List.map List.head t1
+                                |> List.filter (fun c -> not(List.exists ((=)c) (x::y)))
+                                |> List.map (fun c -> c::x::y)
+                                |> fun x -> x @ ps
 
-                    let newAcc = List.fold (fun acc x -> if (List.exists ((=)x) acc) then acc else x::acc) acc tailsN
-                    createNewTails newPaths prods newAcc
+                    let acc = List.fold (fun acc x -> if (List.exists ((=)x) acc) then acc else x::acc) acc t2
+                    createProductions paths prods acc
                 | _ -> acc
-//!!!!!
+
             match prod.Right.Length = 1 && prod.Right.Head.IsVariable && prod.Left = prod.Right.Head with
             | true -> [] 
-            | false -> createNewTails [[prod.Right.Head; prod.Left]] productions []
-                       |> List.map (fun t -> new Production(prod.Left, t))
-                       |> fun ps -> ps @ List.filter ((<>)prod) productions
+            | false -> createProductions [[prod.Right.Head; prod.Left]] productions []
+                       |> List.map (fun x -> new Production(prod.Left, x))
+                       |> fun x -> x @ List.filter ((<>)prod) productions
 
         let rec deleteProductions list =
             let chains = List.tryFind (fun (x:Production) -> x.Right.Length = 1 && x.Right.Head.IsVariable) list
@@ -171,146 +163,101 @@ type private Grammar(start: Character, productions: Production list) =
 
         new Grammar(g.Start, deleteProductions g.Productions)
 
-    //---
-    static member sortAndDeleteDuplicates(grammar : Grammar) =
-        
-            let rec DeleteDuplicates acc prods =
+    static member sortClean(g: Grammar) = 
+        let rec deleteDoubles prods acc =
                 match prods with
-                | p1::p2::ps -> if p1 = p2 then DeleteDuplicates acc (p2::ps)
-                                else DeleteDuplicates (p1::acc) (p2::ps)
-                | [p]        -> DeleteDuplicates (p::acc) []
+                | p1::p2::ps -> match p1 = p2 with
+                                | true -> deleteDoubles (p2::ps) acc
+                                | false -> deleteDoubles  (p2::ps) (p1::acc)
+                | [x]        -> deleteDoubles [] (x::acc)
                 | _          -> List.rev acc
         
-            let newProds = grammar.Productions
-                        |> List.sort
-                        |> DeleteDuplicates [] 
+        new Grammar(g.Start, deleteDoubles (List.sort g.Productions) [] )
 
-            let newGrammar = new Grammar(grammar.Start, newProds)
+    static member deleteUnnecessaryProducts(g:Grammar) =
+        let rec findProductive acc =
+            let tmp = g.Productions
+                    |> List.filter (fun p -> List.forall (fun (c:Character) -> c.IsTerminal || Grammar.CharExists(c,acc)) p.Right)
+                    |> List.map (fun p -> p.Left)
+                    |> List.fold (fun acc x -> Grammar.AddUnique(x,acc)) acc
+            match acc.Length <> tmp.Length with
+            | true  -> findProductive tmp 
+            | false -> acc
+         
+        let rec findAttainable list acc =
+            match list with
+            | x::xs -> g.Productions
+                    |> List.filter (fun p -> p.Left = x)
+                    |> List.map (fun p -> p.Right)
+                    |> List.concat
+                    |> List.filter (fun c -> c.IsVariable)
+                    |> List.filter (fun c -> not(Grammar.CharExists(c,acc)) )
+                    |> fun y -> findAttainable (y@xs) (x::acc)
+            | _     -> acc
 
-            newGrammar
-
-    //--
-    static member deleteNonproductiveProducts(grammar : Grammar) =
+        let productions = List.filter (fun (x:Production) -> Grammar.CharExists(x.Left,findProductive [])) g.Productions
+                        |> List.filter (fun x -> List.forall (fun (c:Character) -> c.IsTerminal || Grammar.CharExists(c,findProductive [])) x.Right)
+                        |> List.filter (fun x -> Grammar.CharExists(x.Left,findAttainable [g.Start] []))
+                        |> List.filter (fun x -> List.forall (fun (c:Character) -> c.IsTerminal || Grammar.CharExists(c,findAttainable [g.Start] [])) x.Right)
         
-            ///////////////////////////////////////////////////////////////////////////////////////////
+        new Grammar(g.Start, productions)  
 
-            let rec getProductiveVariables acc =
-                let newAcc = grammar.Productions
-                            |> List.filter (fun p -> List.forall (fun (c : Character) -> c.IsTerminal || Grammar.CharExists(c,acc)) p.Right)
-                            |> List.map (fun p -> p.Left)
-                            |> List.fold (fun acc x -> Grammar.AddUnique(x,acc)) acc
 
-                if newAcc.Length <> acc.Length then getProductiveVariables newAcc else acc
+    static member shorten(g: Grammar) =
+        let rec splits left (right : Character list) =
+            match right with
+            | x::xs when x.IsVariable -> splits (x::left) xs
+            | _                       -> List.rev left, right
         
-            ///////////////////////////////////////////////////////////////////////////////////////////
+        let rec remove (list:Production list) acc =
+            match list with
+            | x::xs when x.Right.Length <= 1 -> remove xs (x::acc)
+            | x::xs -> let left, right = splits [] x.Right
+                       match right.IsEmpty && x.Right.Length = 2 with
+                       | true -> remove xs (x::acc)
+                       | _ -> let var = new Character("S!" + Convert.ToString( (new System.Random()).Next(1, 1000) ))
+                              match right.IsEmpty with
+                              | true -> remove ((new Production(var, x.Right.Tail))::xs) ((new Production(x.Left, [x.Right.Head; var]))::acc)
+                              | false -> remove ((new Production(x.Left, left @ (var::right.Tail)))::xs) ((new Production(var, [right.Head]))::acc)
+            | _     -> acc
 
-            let productiveVars = getProductiveVariables []
+        new Grammar(g.Start, remove g.Productions [])
 
-            let newProds = grammar.Productions
-                        |> List.filter (fun p -> Grammar.CharExists(p.Left,productiveVars) )
-                        |> List.filter (fun p -> List.forall (fun (c : Character) -> c.IsTerminal || Grammar.CharExists(c,productiveVars)) p.Right)
-            let newGrammar = new Grammar(grammar.Start, newProds)
-
-            newGrammar
-    
-    //---
-    static member deleteUnattainableProducts(grammar : Grammar) =
-        
-            ///////////////////////////////////////////////////////////////////////////////////////////
-
-            let rec getAttainableVariables curr acc =
-                match curr with
-                | c::cs -> grammar.Productions
-                        |> List.filter (fun p -> p.Left = c)
-                        |> List.map (fun p -> p.Right)
-                        |> List.concat
-                        |> List.filter (fun c -> c.IsVariable)
-                        |> List.filter (fun c -> not <| Grammar.CharExists(c,acc))
-                        |> fun a -> getAttainableVariables (a@cs) (c::acc)
-                | _     -> acc
-
-        
-            ///////////////////////////////////////////////////////////////////////////////////////////
-
-            let attainableVars = getAttainableVariables [grammar.Start] []
-
-            let newProds = grammar.Productions
-                        |> List.filter (fun p -> Grammar.CharExists(p.Left,attainableVars))
-                        |> List.filter (fun p -> List.forall (fun (c : Character) -> c.IsTerminal || Grammar.CharExists(c,attainableVars)) p.Right)
-            let newGrammar = new Grammar(grammar.Start, newProds)
-
-            newGrammar         
-          
-        ///////////////////////////////////////////////////////////////////////////////////////////
-
-    static member removeTerminalsAndLongTails(grammar : Grammar) =
-        
-            let rec splitTail left (right : Character list) =
+    static member newAxiom (g:Grammar) =
+            let rec lift left (right:Production list) =
                 match right with
-                | r::rs when r.IsVariable -> splitTail (r::left) rs
-                | _                       -> List.rev left, right
-        
-            let rec removeTermssAndLongTails (prods : Production list) acc =
-                match prods with
-                | p::ps when p.Right.Length <= 1 -> removeTermssAndLongTails ps (p::acc)
-                | p::ps -> let left, right = splitTail [] p.Right
-                           if right.IsEmpty && p.Right.Length = 2 then removeTermssAndLongTails ps (p::acc)
-                           else 
-                                let newName = "S!" + Convert.ToString( (new System.Random()).Next(1, 1000) )
-                                let newVar = new Character(newName)
-
-                                let p1, p2 = 
-                                    if not right.IsEmpty then      
-                                        new Production(p.Left, left @ (newVar::right.Tail)),
-                                        new Production(newVar, [right.Head])
-                                    else 
-                                        new Production(newVar, p.Right.Tail),
-                                        new Production(p.Left, [p.Right.Head; newVar])
-
-                                removeTermssAndLongTails (p1::ps) (p2::acc)
-                | _     -> acc
-
-            let newProds = removeTermssAndLongTails grammar.Productions []
-            let newGrammar = new Grammar(grammar.Start, newProds)
-
-            newGrammar
-
-        ///////////////////////////////////////////////////////////////////////////////////////////
-
-    static member moveAxiomUp (grammar : Grammar) =
-            let rec moveAxUp left (right : Production list) =
-                match right with
-                | r::rs when r.Left = grammar.Start -> r::(List.rev left @ rs)
-                | r::rs                             -> moveAxUp (r::left) rs
+                | x::xs when x.Left = g.Start -> x::(List.rev left@xs)
+                | x::xs -> lift (x::left) xs
                 | _ -> []
 
-            if grammar.Productions.IsEmpty then grammar
-            else let newProds = moveAxUp [] grammar.Productions
-                 let newGrammar = new Grammar(grammar.Start, newProds)
-                 newGrammar
-    //--
+            match g.Productions with
+            | [] -> g
+            | _  -> new Grammar(g.Start, lift [] g.Productions)
+
     static member ToCNF(g: Grammar) =
         Grammar.DeleteEpsilons(g)
         |> Grammar.deleteChains
-        |> Grammar.sortAndDeleteDuplicates
-        |> Grammar.deleteNonproductiveProducts
-        |> Grammar.deleteUnattainableProducts
-        |> Grammar.removeTerminalsAndLongTails
-        |> Grammar.sortAndDeleteDuplicates
-        |> Grammar.moveAxiomUp
-//!!!!!
+        |> Grammar.sortClean
+        |> Grammar.deleteUnnecessaryProducts
+        |> Grammar.shorten
+        |> Grammar.sortClean
+        |> Grammar.newAxiom
+
             
 [<EntryPoint>]
 let main argv = 
-    let G = Grammar.ParseFromFile "../../tests/cnf02.g"
+    (*
+    let G = Grammar.ParseFromFile "../../tests/cnf01.g"
     let cnfG = Grammar.ToCNF(G.Value)
     printfn "%A" cnfG
     0
-    (*
+    *)
     if argv.Length <> 1 then printfn "Incorrect arguments"
-        else try let gramOpt = Grammar.ParseFromFile argv.[0] 
-                 gramOpt
+        else try let g = Grammar.ParseFromFile argv.[0]
+                 match g with
+                 | None -> printfn "FAIL"
+                 | Some x -> printfn "%A" <| Grammar.ToCNF(x)
              with
              | :? FileNotFoundException -> printfn "File not found"
     0
-    *)
+    
