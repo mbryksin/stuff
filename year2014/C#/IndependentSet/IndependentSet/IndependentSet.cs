c5489d0cfadd66b98f81e0e4d1d9e7e83f8fae27
mbryksin <matvey.bryksin@gmail.com>
2014-11-04 22:48:12 +0300

    create project

---
+++ year2014/C#/IndependentSet/IndependentSet/IndependentSet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuickGraph;
+
+namespace Algorithms
+{
+    public class IndependentSet
+    {
+        public static int Robson(List<int> vertices, SEdge<int>[] edges)
+        {
+            var graph = new AdjacencyGraph<int, SEdge<int>>();
+            return 0;
+        }
+    }
+}
